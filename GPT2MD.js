1a74f05d10ebff4dbf3083c28dc0ee53f1669c7d
Efer <mehdi@skiff.com>
2023-11-01 20:46:47 +0330

    The Very First Commit

---
+++ GPT2MD.js
@@ -0,0 +1,55 @@
+function SaveChatGPTtoMD() {
+	const chatMessages = document.querySelectorAll(".text-base.p-4");
+	const pageTitle = document.title;
+	const now = new Date();
+	const dateString = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}-${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}-${now.getSeconds().toString().padStart(2, '0')}`;
+	let fileName = "ChatGPT - " + pageTitle + ' - ' + dateString + ".md";
+    let markdownContent = "---\nmarp: false\n---\n";
+    
+	for (const message of chatMessages) {
+		let Revision = '';
+        const revisionElement = message.querySelector(".text-xs .flex-grow");
+        
+		if (revisionElement && revisionElement.innerHTML) {
+			Revision = `Edit Revision: **${revisionElement.innerHTML}**\n`;
+        }
+        
+		if (message.querySelector(".whitespace-pre-wrap")) {
+			let messageText = message.querySelector(".whitespace-pre-wrap").innerHTML;
+			const sender = message.querySelector("img") ? "You" : "ChatGPT";
+			// adds Escapes to non-MD
+			messageText = messageText.replace(/_/gs, "\_").replace(/\*/gs, "\*").replace(/\^/gs, "\^").replace(/~/gs, "\~"); // I debated adding #, > (blockquotes), and | (table)
+			// <p> element and everything in-line or inside
+			messageText = messageText.replace(/<p>(.*?)<\/p>/g, function(match, p1) {
+				return '\n' + p1.replace(/<b>(.*?)<\/b>/g, '**$1**').replace(/<\/?b>/g, "**").replace(/<\/?i>/g, "_").replace(/<code>/g, " `").replace(/<\/code>/g, "` ") + '\n';
+			});
+			markdownContent += `**${sender}:** ${Revision}${messageText.trim()}\n\n---\n---\n\n`;
+		}
+    }
+    
+	// Remove Span with only class declaration, there is nesting? If there is more than 5 layers, just do it manually
+	const repeatSpan = /<span class="[^"]*">([^<]*?)<\/span>/gs;
+    markdownContent = markdownContent.replace(repeatSpan, "$1").replace(repeatSpan, "$1").replace(repeatSpan, "$1").replace(repeatSpan, "$1").replace(repeatSpan, "$1");
+    
+	// Code Blocks, `text` is the default catch-all (because some commands/code-blocks aren't styled/identified by ChatGPT yet)
+	markdownContent = markdownContent.replace(/<pre>.*?<code[^>]*>(.*?)<\/code>.*?<\/pre>/gs, function(match, p1) {
+		const language = match.match(/class="[^"]*language-([^"\s]*)[^"]*"/);
+		const languageIs = language ? language[1] : 'text';
+		return '\n``` ' + languageIs + '\n' + p1 + '```\n';
+	});
+
+	markdownContent = markdownContent.replace(/<p>(.*?)<\/p>/g, function(p1) {
+		return '\n' + p1.replace(/<b>(.*?)<\/b>/g, '**$1**').replace(/<\/?b>/g, "**").replace(/<\/?i>/g, "_").replace(/<code>/g, " `").replace(/<\/code>/g, "` ") + '\n';
+    });
+    
+    markdownContent = markdownContent.replace(/<div class="markdown prose w-full break-words dark:prose-invert dark">/gs, "").replace(/\r?\n?<\/div>\r?\n?/gs, "\n").replace(/\*\*ChatGPT:\*\* <(ol|ul)/gs, "**ChatGPT:**\n<$1").replace(/&gt;/gs, ">").replace(/&lt;/gs, "<").replace(/&amp;/gs, "&");
+    
+	const downloadLink = document.createElement("a");
+	downloadLink.download = fileName;
+	downloadLink.href = URL.createObjectURL(new Blob([markdownContent], {
+		type: "text/markdown"
+	}));
+	downloadLink.style.display = "none";
+	document.body.appendChild(downloadLink);
+	downloadLink.click();
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# ChatGPT_To_markdown
-GPT2MD is a neat way to convert your chats with OpenAI - ChatGPT to Markdown. Just inject GPT2MD.js and then call the `SaveChatGPTtoMD` function.
+# OpenAI chat (ChatGPT) to Markdown or TXT
+
+![Header](./Assets/GPT2MD.png)
+
+### GPT2MD is a neat way to convert your chats with OpenAI - ChatGPT to Markdown. Just inject GPT2MD.js and then call the `SaveChatGPTtoMD` function.
+
+You can use some third parties like <a href="https://chrome.google.com/webstore/detail/scripty-javascript-inject/milkbiaeapddfnpenedfgbfdacpbcbam"><img style="display: inline-block" src="./Assets/Scripty.png" width="20" height="20"><b> Scripty</b></a> to inject a JS script to your DOM.
+
+Also, a fascinating idea is setting an onkeypress event handler directly on the window object so that when particular key combinations are pressed, the function is called.
+```js
+window.onkeypress = function(event) {
+	if (event.keyCode == 68) { // keyCode 68 stands for ctrl+D
+		SaveChatGPTtoMD();
+	}
+}
+```
+
+Method No. 2: Using event listeners
+```js
+window.addEventListener('keypress', function(event) {
+	if (event.keyCode == 69) { // keyCode 69 stands for ctrl+E
+		SaveChatGPTtoTXT();
+	}
+});
+```
+
+<br>
+
+You can also change the code to simultaneously change the ready-to-download variable and, using desired key combinations, save the output file as either TXT or MD.
+
+It would be nice to add a lightweight browser extension to make it easier for you to use this tool and easily convert your chat history with ChatGPT into your preferred outputs (TXT, MD, PDF, etc.) ASAP.
